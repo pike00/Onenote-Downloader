18b1d2423898f9ff8dc02b26cb97f8be8b8df922
Will Pike <willpike00@gmail.com>
2020-11-02 12:00:42 -0500

    Updated HTTPRequestHandler to show resposne

--- OAuth/OAuthHTTPRequestHandler.py
+++ OAuth/OAuthHTTPRequestHandler.py
@@ -1,24 +1,44 @@
 import logging
-import time
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from furl import furl
-import threading
+import re
+from http.server import BaseHTTPRequestHandler
 
-logging.basicConfig(level = logging.ERROR)
+from furl import furl
+
+logging.basicConfig(level=logging.ERROR)
 
 
 class OAuthHTTPRequestHandler(BaseHTTPRequestHandler):
-
 
 
     def log_message(self, format, *args):
         pass
 
     def do_GET(self):
+        response = """Content-type: text/html
+
+            <html>
+            <head>
+            <title>Authorization Complete</title>
+            </head>
+
+            <body>
+            Authorization with OneNote Downloader complete. You can close this page.
+            <br/><br/><br/>
+            </body>
+            </html>
+            """
         self.send_response(200)
         self.end_headers()
 
         f = furl(self.path)
 
+        code = ""
+
         if f.path == furl('/onenote_downloader'):
-            self.server.set_code(f.args['code'])
+            code = f.args['code']
+            self.server.set_code(code)
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+
+        self.wfile.write(str.encode(self.server.get_code()))
