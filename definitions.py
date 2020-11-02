d6235b2ec49812d0b198280a6d9e4f6cc64ecc21
Will Pike <willpike00@gmail.com>
2020-11-02 11:28:29 -0500

    Extracted URLs to definitions.py

--- definitions.py
+++ definitions.py
@@ -12,3 +12,6 @@ TOKEN_PATH = join(KEYS_DIR, "token.json")
 OUT_DIR = join(ROOT_DIR, "out/")
 
 os.makedirs(OUT_DIR, exist_ok = True)
+TOKEN_URL = "https://login.microsoftonline.com/organizations/oauth2/v2.0/token"
+GRAPH_URL = "https://graph.microsoft.com/v1.0/me/onenote/"
+AUTHORIZATION_URL = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize"
