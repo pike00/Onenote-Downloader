22e7ef353fc162d3fd5cd97a966188bf48505e9d
Will Pike <willpike00@gmail.com>
2020-11-03 08:57:02 -0500

    Wrapped main code in a try/except loop and while to continue running

--- onenote_dl/download_onenote.py
+++ onenote_dl/download_onenote.py
@@ -1,9 +1,11 @@
 import re
+import time
 from os import makedirs
 
 
 # Get Notebooks URL
 from furl import furl
+from tqdm import tqdm
 
 from definitions import *
 from onenote_dl.helpers import *
@@ -14,75 +16,72 @@ f /= "notebooks"
 # Download and write the notebooks json to the onenote_dl json folder
 notebooks_js = download_and_save_json_to_file([OUT_DIR, "notebooks.json"], f.url)
 
-# Loop over each notebook
-for notebook_js in notebooks_js:
-    notebook_id = notebook_js['id']
+while True:
 
-    # Create notebook directory within JSON directory
-    notebook_dir = join(OUT_DIR, notebook_id)
-    makedirs(notebook_dir, exist_ok=True)
+    try:
+        # Loop over each notebook
+        for notebook_js in notebooks_js:
+            notebook_id = notebook_js['id']
 
-    # Download and save each subsection url and related json
-    sections_js = download_and_save_json_to_file([notebook_dir, "sections.json"],
-                                                 notebook_js['sectionsUrl'])
+            # Create notebook directory within JSON directory
+            notebook_dir = join(OUT_DIR, notebook_id)
+            makedirs(notebook_dir, exist_ok=True)
 
-    # Loop over each section
-    for section_js in sections_js:
+            # Download and save each subsection url and related json
+            sections_js = download_and_save_json_to_file([notebook_dir, "sections.json"],
+                                                         notebook_js['sectionsUrl'])
 
-        section_id = section_js['id']
+            # Loop over each section
+            for section_js in sections_js:
 
-        # Create section directory within each notebook directory
-        section_dir = join(notebook_dir, section_id)
-        makedirs(section_dir, exist_ok=True)
+                section_id = section_js['id']
 
-        # Download and Save pages json for each section
-        pages_js = download_and_save_json_to_file([section_dir, "pages.json"], section_js['pagesUrl'])
+                # Create section directory within each notebook directory
+                section_dir = join(notebook_dir, section_id)
+                makedirs(section_dir, exist_ok=True)
 
-        # Loop over each page within each section
-        for page_js in pages_js:
+                # Download and Save pages json for each section
+                pages_js = download_and_save_json_to_file([section_dir, "pages.json"], section_js['pagesUrl'])
 
-            page_id = page_js['id']
+                # Loop over each page within each section
+                for page_js in pages_js:
 
-            # Download page_html
-            contentUrl = page_js['contentUrl']
+                    page_id = page_js['id']
 
-            # noinspection SpellCheckingInspection
-            page_html = get_graph_page(contentUrl + f"?includeinkML=true")
-            # Don't save yet - must replace asset urls with filenames
+                    # Download page_html
+                    contentUrl = page_js['contentUrl']
 
-            # Create a page directory for assets
-            page_dir = join(section_dir, page_id)
-            makedirs(page_dir, exist_ok=True)
+                    # noinspection SpellCheckingInspection
+                    page_html = get_graph_page(contentUrl + f"?includeinkML=true")
+                    # Don't save yet - must replace asset urls with filenames
 
-            # Find resource urls within each page
-            # Includes images
-            asset_urls = re.findall(r'"(https?://.+?\$value)"', page_html)
-            for asset_url in asset_urls:
-                uuid = asset_url.split("/")[7]
-                asset_filename = join(page_dir, uuid)
+                    # Create a page directory for assets
+                    page_dir = join(section_dir, page_id)
+                    makedirs(page_dir, exist_ok=True)
 
-                # Download asset if doesn't exist
-                if not os.path.exists(asset_filename):
-                    asset = download_and_write_binary_to_file(asset_filename, asset_url)
+                    # Find resource urls within each page
+                    # Includes images
+                    asset_urls = re.findall(r'"(https?://.+?\$value)"', page_html)
+                    for asset_url in tqdm(asset_urls):
+                        uuid = asset_url.split("/")[7]
+                        asset_filename = join(page_dir, uuid)
 
-                # Replace the page html that has the asset_urls with the filenames for local loading
-                page_html = page_html.replace(asset_url, asset_filename.split("\\")[-1])
+                        # Download asset if doesn't exist
+                        if not os.path.exists(asset_filename):
+                            asset = download_and_write_binary_to_file(asset_filename, asset_url)
 
-            # Now save page html to page folder
-            with open(join(page_dir, "page.html"), "w") as file:
-                file.write(page_html)
-                print(f"Wrote {page_id}.html")
+                        # Replace the page html that has the asset_urls with the filenames for local loading
+                        page_html = page_html.replace(asset_url, asset_filename.split("\\")[-1])
 
-                # img_tags = re.findall(r'<img (.*) />', content)
-            # for img_tag in img_tags:
-            #     attrs = img_tag.split(" ")
-            #     img_src = ""
-            #     for attr in attrs:
-            #         if "data-fullres-src-type" in attr:
-            #             img_type = re.findall(r'"(.*)"', attr)[0]
-            #         elif "data-fullres-src" in attr:
-            #             img_src = re.findall(r'"(.*)"', attr)[0]
-            #
-            #     # print(f"Image: {img_type} \t {img_src}")
+                    # Now save page html to page folder
 
-            # print(filename)
+                    page_filename = join(page_dir, "page.html")
+                    if not os.path.exists(page_filename):
+                        with open(page_filename, "w", encoding='utf-8') as file:
+                            file.write(page_html)
+                            print(f"Wrote {page_id}.html")
+
+    except:
+        print("exception caught. Waiting 60 seconds")
+
+    time.sleep(600)
