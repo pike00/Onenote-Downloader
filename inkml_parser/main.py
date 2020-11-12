f6d517d831a6ce30783ffbc3b17fa96e37b2f525
Will Pike <willpike00@gmail.com>
2020-11-11 21:52:14 -0500

    Add working inkml renderer

--- inkml_parser/main.py
+++ inkml_parser/main.py
@@ -1,3 +1,18 @@
 from inkml_parser.inkml_render import get_traces_data, inkml2img
+from inkml_parser.inkml_util import get_max_dimensions
 
-inkml2img('C:\\Users\\Will Pike\\Downloads\\inkml.inkml.xml', "C:\\Users\\Will Pike\\Downloads\\")
+x, y = get_max_dimensions("tmp.inkml")
+
+# var
+# pixel = (himetric * dpi) / 2540;
+#
+# // XPS
+# DPI = 141.21
+# // Himetric
+# max = 32767
+# // Pixels = 32767 * 141.21 / 2540
+
+x = x * 141.21 / 2540 / 2
+y = y * 141.21 / 2540 / 2
+
+print(f"{x} {y}")
