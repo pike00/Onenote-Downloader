8970fd9db33fb9209decf4307e0e587ecc147b1c
Will Pike <willpike00@gmail.com>
2020-11-11 21:54:09 -0500

    Change grammar of helper functions

--- onenote_dl/helpers.py
+++ onenote_dl/helpers.py
@@ -14,32 +14,32 @@ def get_graph_page(req_url):
 
 
 # Returns json_content
-def download_and_save_json_to_file(filename_array, req_url, overwrite=False) -> json:
+def get_json(filename, req_url, forceDownload=False) -> json:
     refresh_token()
     json_content = json.loads(get_graph_page(req_url))['value']
-
-    filename = ""
-    for filename_part in filename_array:
-        filename = join(filename, filename_part)
 
     # Append ".json" if not already in filename
     if ".json" not in filename:
         filename += ".json"
 
     # Check if the file already exists and you don't want to overwrite
-    if exists(filename) and not overwrite:
+    if exists(filename) and not forceDownload:
+
         with open(filename, "r") as file:
             return json.load(file)
 
+
+
+    return json_content
+
+
+def write_json(filename_array, json_content):
     # Otherwise, write the json formatting nicely
     str_content = json.dumps(json_content, indent=2, sort_keys=True)
     with open(filename, "w") as file:
         file.write(str_content)
 
     print(f"Wrote {filename}")
-
-    return json_content
-
 
 def download_and_write_binary_to_file(filename, req_url):
     refresh_token()
